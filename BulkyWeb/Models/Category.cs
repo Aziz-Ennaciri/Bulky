c28f14da5dc3caa16dc60aa46723284a2436a29a
ENNACIRI <ENNACIRI@PC-ENNACIRI>
2024-11-15 15:03:16 +0100

    creating category

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
     public class Category
     {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+
     }
 }
