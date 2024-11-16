d07cfcc13b5a2053c1ba96cde9a65fde909737c3
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-11-16 21:36:05 +0100

    Seeding data in my DB for testing

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -10,5 +10,14 @@ namespace BulkyWeb.Data
             
         }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { id=1,Name="Action",DisplayOrder=1 },
+                new Category { id=2,Name="shi",DisplayOrder=2 },
+                new Category { id=3,Name="faa",DisplayOrder=3 }
+                );
+        }
     }
 }
