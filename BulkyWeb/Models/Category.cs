f04371747f36b86235eb3a2e54b3da38c13fc233
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-11-16 21:36:29 +0100

    Modifying Category

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -8,7 +8,7 @@ namespace BulkyWeb.Models
         public int id { get; set; }
         [Required]
         public string Name { get; set; }
-        public string DisplayOrder { get; set; }
+        public int DisplayOrder { get; set; }
 
     }
 }
