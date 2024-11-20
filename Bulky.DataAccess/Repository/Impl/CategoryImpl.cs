a0d6eee93e698f2b42261493350fdbe8c5df9977
ENNACIRI <ENNACIRI@PC-ENNACIRI>
2024-11-20 16:22:12 +0100

    Creating CategoryImpl class for implementing metods from ICategoryRepository

---
+++ Bulky.DataAccess/Repository/Impl/CategoryImpl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BulkyDataAccess.Data;
+using BulkyDataAccess.Repository.Interf;
+using BulkyModels.Category;
+
+namespace BulkyDataAccess.Repository.Impl
+{
+    public class CategoryImpl : Repository<Category> , ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryImpl(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
