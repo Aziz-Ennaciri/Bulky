9e40f38a200fa0149be533fdc1908e01f01521cb
ENNACIRI <ENNACIRI@PC-ENNACIRI>
2024-11-20 16:41:17 +0100

    Adding ICategoryRepository for DI using Scoped

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,4 +1,6 @@
 using BulkyDataAccess.Data;
+using BulkyDataAccess.Repository.Impl;
+using BulkyDataAccess.Repository.Interf;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICategoryRepository,CategoryImpl>();
 
 var app = builder.Build();
 
