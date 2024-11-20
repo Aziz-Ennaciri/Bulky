43237d53aab0627493231ea13d728f09d4fd5cbc
ENNACIRI <ENNACIRI@PC-ENNACIRI>
2024-11-20 17:51:31 +0100

    change the DI from ICategoryRepository to  IUnitOfWork

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<ICategoryRepository,CategoryImpl>();
+builder.Services.AddScoped<IUnitOfWork,UnitOfWorkImpl>();
 
 var app = builder.Build();
 
