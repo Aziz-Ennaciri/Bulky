1b55c8419e1847a7323806d48b28ab475c4cc021
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-11-22 00:07:33 +0100

    .... ... ..

---
+++ Bulky.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,16 @@
+﻿using BulkyModels.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyModels.ViewModels
+{
+    public class ProductVM
+    {
+        public Product product {  get; set; }
+        public IEnumerable<SelectListItem> categoryList { get; set; }
+    }
+}
