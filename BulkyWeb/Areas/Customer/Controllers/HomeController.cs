ab3eb74bdb58f6ae8cd727fe6e1af3f50ca1ad5a
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-11-24 13:37:00 +0100

    updating the style of home page to display the products in my DB , and adding for each product and details page with all details of that product

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using BulkyDataAccess.Repository.Interf;
 using BulkyModels.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +9,23 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.ProductRepo.GetAll(includeProperties:"Category");
+            return View(products);
+        }
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.ProductRepo.GetFirstIdOrDefault(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
