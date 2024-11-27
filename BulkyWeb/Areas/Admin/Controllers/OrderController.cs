85cb1056a9bcc15e8b4b800e4306685938279829
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-11-27 20:32:54 +0100

    Adding OrderController

---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using BulkyDataAccess.Repository.Interf;
+using BulkyModels.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;   
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeaderRepo.GetAll(includeProperties: "ApllicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+
+        #endregion
+    }
+}
