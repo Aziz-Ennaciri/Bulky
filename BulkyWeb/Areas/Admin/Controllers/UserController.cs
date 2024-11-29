a734bee98c617dcd7c60e5eacc27cc5a79b509bf
ENNACIRI <ENNACIRI@PC-ENNACIRI>
2024-11-29 16:32:09 +0100

    Add a section for manage user and their roles , and diplaying and dataTable

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using BulkyDataAccess.Data;
+using BulkyDataAccess.Repository.Interf;
+using BulkyModels;
+using BulkyModels.Models;
+using BulkyModels.ViewModels;
+using BulkyUtility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = StaticDetails.Role_Company)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET: UserController
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+            #region API CALLS
+            [HttpGet]
+            public IActionResult GetAll()
+            {
+                List<ApplicationUser> objUserList = _db.applicationUsers.Include(u=>u.company).ToList();
+                return Json(new { data = objUserList });
+            }
+
+            [HttpDelete]
+            public IActionResult Delete(int? id)
+            {
+                return Json(new { success = true, message = "Delete Successful" });
+            }
+            #endregion
+
+
+
+        }
+    } 
+
---
+++ BulkyWeb/wwwroot/js/user.js
@@ -0,0 +1,41 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            url: '/admin/user/getall'
+        },
+        "columns": [
+            { data: 'name', "width": "25%" },
+            { data: 'email', "width": "15%" },
+            { data: 'phoneNumber', "width": "10%" },
+            { data: 'company.name', "width": "15%" },
+            { data: 'role', "width": "10%" },
+            {
+                data: 'id', "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                                <a href="/admin/company/upsert?id=${data}" class="btn btn-primary mx-2"><i class ="bi bi-pencil-square"></i>Edit</a>
+                            </div>`
+                }, "width": "25%"
+            }
+            //{
+            //    data: null,
+            //    width: "10%",
+            //    render: function (data, type, row) {
+            //        return `
+            //            <a href="/Admin/Product/Upsert/${row.id}" class="btn btn-success btn-sm">
+            //                <i class="bi bi-pencil-square"></i> Edit
+            //            </a>
+            //            <a href="#" class="btn btn-danger btn-sm" onclick="deleteProduct(${row.id})">
+            //                <i class="bi bi-trash"></i> Delete
+            //            </a>
+            //        `;
+            //    }
+            //}
+        ]
+    });
+}
